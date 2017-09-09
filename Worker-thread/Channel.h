612b5c5b5996d77deeb5ae1a433c1048f5e9c4ae
L-Slavov <lubomir93x@gmail.com>
2017-09-09 16:53:31 +0300

    Added closing logic to Channel

--- Worker-thread/Channel.h
+++ Worker-thread/Channel.h
@@ -16,9 +16,14 @@ private:
     std::queue<item> queue;
     std::mutex mu;
     std::condition_variable cv;
+    bool closed;
 public:
+    Channel() : closed(false) { }
     void put(const item &i){
         std::unique_lock<std::mutex> lock (mu);
+        if(closed){
+            throw std::logic_error("put to closed channel");
+        }
         queue.push(i);
         cv.notify_one();
     }
@@ -31,6 +36,15 @@ public:
     }
     bool empty(){
         return queue.empty();
+    }
+    void close() {
+        std::unique_lock<std::mutex> lock(mu);
+        closed = true;
+        cv.notify_all();
+    }
+    bool is_closed() {
+        std::unique_lock<std::mutex> lock(mu);
+        return closed;
     }
 
 };
